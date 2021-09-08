ecf390b5e9e7bff15a81ad4f9e426843f5bef8fb
gabidron <drongabi8@gmail.com>
2021-09-08 11:09:31 +0300

    firebase setup

---
+++ amazone-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALt8CQIlzxpAJ9FEGZkElznyX23EeuBkE",
+    authDomain: "clone-e73b5.firebaseapp.com",
+    projectId: "clone-e73b5",
+    storageBucket: "clone-e73b5.appspot.com",
+    messagingSenderId: "352804969081",
+    appId: "1:352804969081:web:e6442778ea2e4b456492a8"
+  };
