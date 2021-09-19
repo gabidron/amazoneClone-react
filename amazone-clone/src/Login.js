cbd0336dab50d5377cef2769cfad6a644bf63e94
gabidron <drongabi8@gmail.com>
2021-09-19 17:41:36 +0300

    setup firebase for login && register

--- amazone-clone/src/Login.js
+++ amazone-clone/src/Login.js
@@ -6,6 +6,14 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -32,14 +40,20 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          <button className="login__singInButton">Sign In</button>
+          <button
+            type="submit"
+            className="login__singInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By singning-in you agree to the Best Amazon Fake CLONE Conditions of
           Use & Sale. Please see out Privacy Notice, our Cookies Notice and our
           Internet-Based Ads
         </p>
-        <button className="login__registerButton">
+        <button className="login__registerButton" onClick={register()}>
           Create your Amazon Account
         </button>
       </div>
--- amazone-clone/src/firebase.js
+++ amazone-clone/src/firebase.js
@@ -1,8 +1,16 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyALt8CQIlzxpAJ9FEGZkElznyX23EeuBkE",
-    authDomain: "clone-e73b5.firebaseapp.com",
-    projectId: "clone-e73b5",
-    storageBucket: "clone-e73b5.appspot.com",
-    messagingSenderId: "352804969081",
-    appId: "1:352804969081:web:e6442778ea2e4b456492a8"
-  };
+  apiKey: "AIzaSyALt8CQIlzxpAJ9FEGZkElznyX23EeuBkE",
+  authDomain: "clone-e73b5.firebaseapp.com",
+  projectId: "clone-e73b5",
+  storageBucket: "clone-e73b5.appspot.com",
+  messagingSenderId: "352804969081",
+  appId: "1:352804969081:web:e6442778ea2e4b456492a8",
+};
+
+const firebaseApp = firebase.inializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
